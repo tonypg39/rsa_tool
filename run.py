5723f8aada287b15c6e7a0ba849250f080a26477
Juan Antonio Pinera Garcia <tonypinera.2015@gmail.com>
2023-09-22 09:01:35 +0200

    Add some test files to help with development

--- run.py
+++ run.py
@@ -25,9 +25,17 @@ if __name__ == "__main__":
     cmdline.add_argument('--name', default="")
     cmdline.add_argument('--id', default="")
     cnt_name = "generic"
-    cmd_base = f"/home/tony/Documents/software/ros_docker/gui-docker --name {cnt_name} ur10dev:v1 roslaunch ur_gazebo ur10_bringup.launch"
+
+    # print(dck_cmd)
+    cmd_base = f"/home/tony/Documents/software/ros_docker/gui-docker --name {cnt_name} ur10dev:v1 roslaunch ur_gazebo test3.launch"
+    # cmd_base = f"/home/tony/Documents/software/ros_docker/gui-docker --name {cnt_name} ur10dev:v1 sleep infinity"
     os.popen(cmd_base)
-    # gz = "roslaunch ur_gazebo ur10_bringup.launch"
-    # os.popen("docker exec generic 'roslaunch ur_gazebo ur10_bringup.launch'")
 
 
+# docker cp 260de001e0b9:/opt/ros/noetic/share/ur_gazebo/launch/ur10_bringup.launch ./work/
+# docker cp 260de001e0b9:/opt/ros/noetic/share/ur10_moveit_config/launch/moveit_planning_execution.launch ./work/
+# docker cp 260de001e0b9:/opt/ros/noetic/share/ur10_moveit_config/launch/moveit_rviz.launch ./work/
+
+# roslaunch ur_gazebo ur10_bringup.launch
+# roslaunch ur10_moveit_config moveit_planning_execution.launch sim:=true
+# roslaunch ur10_moveit_config moveit_rviz.launch config:=true
